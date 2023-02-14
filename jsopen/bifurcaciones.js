d5c0ce90acb67db8a798037ae7ddd28bc00d5752
arkaitzna <arkaitzara@gmail.com>
2023-02-14 13:31:20 +0100

    Create bifurcaciones.js

---
+++ jsopen/bifurcaciones.js
@@ -0,0 +1,30 @@
+//Bifurcaciones if...else
+
+if (true){
+    console.log("Es verdader")
+}
+
+if(false){
+    console.log("Es verdadero")
+} else{
+    console.log("es falso")
+}
+
+
+let saldo=50;
+let efectivo=60;
+
+
+if (efectivo<=saldo){
+    console.log("Puede retirar dinero")
+} else{
+    console.log("No tiene saldo suficiente")
+}
+
+//If else + if else
+let nota= 5;
+
+if(nota===5){
+    
+}
+
