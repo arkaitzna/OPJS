e7e284e9724d942b9d6e5309f9be818b5500669a
arkaitzna <arkaitzara@gmail.com>
2023-02-14 21:44:02 +0100

    j

--- jsopen/bifurcaciones.js
+++ jsopen/bifurcaciones.js
@@ -22,9 +22,53 @@ if (efectivo<=saldo){
 }
 
 //If else + if else
-let nota= 5;
 
-if(nota===5){
-    
+
+
+    let nota= 2;
+
+    if(nota===5){
+        console.log('Enorabuena, has obtenido un sobresaliente')
+    }
+    else if(nota===4){
+        console.log("Sigue así, has obtenido un notable")
+    }
+    else if(nota===3){
+        console.log("Has aprovado, sigue estudiando")
+    }
+    else if(nota<3){
+        console.log('Tienes que aplicarte mas. Has suspendido')
+    }
+    else{
+        console.log("Ha habido un error, ponte en contacto con tu professor. Esperamos que te haya ido bien el examen y discupa por las molestias!")
+    }
+
+
+    //SWITCH
+
+
+let prueba=4;
+
+switch(prueba){
+    case 1:
+        console.log("Examen nulo");
+        break;
+    case 2:
+        console.log("Has suspendido, estudia más");
+        break;
+    case 3:
+        console.log("Has aprobado, esperemos que la proxima puedas sacar mejor nota");
+        break;
+    case 4:
+        console.log("Felicidades, has sacado un notable!");
+        break;
+    case 5:
+        console.log("Enorabuena, has conseguido un sobresaliente!");
+        break;
+    default:
+        console.log("Error!")
+        break;
 }
+
+
 
---
+++ jsopen/comparaciones.js
@@ -0,0 +1,11 @@
+//Comparaciones
+//Igualdad
+
+let a = 5 
+let b = 5
+let c = 5
+
+if (b>=c && c <= a && a<=c){
+    console.log(" son iguales")
+}
+
